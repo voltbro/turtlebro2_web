fdd2083a30585b3fd787890763cab538042f1141
Ubercola <cola@cactus.ru>
2023-12-08 19:08:52 +0300

    add web files and launch

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'turtlebro_web'
 
@@ -7,9 +9,13 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        # ('share/ament_index/resource_index/packages',
+            # ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # (os.path.join('share', package_name), ['launch/web_server.xml']),
+        # (os.path.join('share', package_name), 'launch/web_server.xml')
+        ('share/' + package_name + '/launch', glob('launch/*'))
+        # ('.', glob('launch/*'))
     ],
     install_requires=['setuptools','flask'],
     include_package_data=True,
--- turtlebro_web/web_server.py
+++ turtlebro_web/web_server.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, send_from_directory, send_file, request, jsonify, render_template
 app = Flask(__name__)
 
+www_path = "../resource/web"
+app = Flask(__name__, static_folder=www_path + '/static', template_folder=www_path)
+
 @app.route("/")
-def hello():
-  return "Hello World1!"
+def serve_index():
+  return render_template('index.html', ros_host = "192.168.0.148")
+#   return "Hello World1!"
 
 def main():
   app.run(host="0.0.0.0", port=8080, threaded=True)
