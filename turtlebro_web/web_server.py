76723da8b5fed35f53fae97e352750da41cd5854
Ubercola <cola@cactus.ru>
2023-12-11 19:30:48 +0300

    Add video topics search

--- turtlebro_web/web_server.py
+++ turtlebro_web/web_server.py
@@ -1,12 +1,32 @@
+import rclpy
+import os
+from rclpy.node import Node
 from flask import Flask, send_from_directory, send_file, request, jsonify, render_template
-app = Flask(__name__)
+
+ROS_DOMAIN_ID :int = int(os.environ.get('ROS_DOMAIN_ID',0))
+rclpy.init(domain_id=ROS_DOMAIN_ID)
+ros_web_node = Node("ros_web_node")
 
 www_path = "../resource/web"
 app = Flask(__name__, static_folder=www_path + '/static', template_folder=www_path)
 
+
+def get_video_topics(node: Node):
+    
+    topics = []
+    for topic in node.get_topic_names_and_types():
+        if topic[1] == ['sensor_msgs/msg/Image']:
+            # topics.append(topic[0].replace('/compressed',''))
+            topics.append(topic[0])
+
+    return topics        
+
+
 @app.route("/")
 def serve_index():
-  return render_template('index.html', ros_host = "192.168.0.148")
+  return render_template('index.html', 
+                        ros_host = "192.168.0.148",
+                        video_topics = get_video_topics(ros_web_node))
 #   return "Hello World1!"
 
 def main():
